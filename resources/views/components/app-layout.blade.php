379ea44f2053c889a0a950e9ec83896694682b86
brayan <brayan-albornoz@debian12.myguest.virtualbox.org>
2024-12-06 17:41:11 -0600

    Logos

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,12 +6,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ela Admin - HTML5 Admin Template</title>
+    <title>SportXtreme -- Tienda de deportes</title>
     <meta name="description" content="Ela Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+    <link rel="apple-touch-icon" href="images/milogo2.png">
+    <link rel="shortcut icon" href="images/milogo2.png">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -161,7 +161,7 @@
             <div class="top-left">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="./inicio">
-                        <img src="{{ asset('images/milogo.jpg') }}" alt="Logo" width="75" height="auto">
+                        <img src="{{ asset('images/milogo2.png') }}" alt="Logo" width="75" height="auto">
                     </a>
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
--- resources/views/vistas/login.blade.php
+++ resources/views/vistas/login.blade.php
@@ -6,12 +6,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ela Admin - HTML5 Admin Template</title>
-    <meta name="description" content="Ela Admin - HTML5 Admin Template">
+    <title>SportXtreme -- Tienda de deportes</title>
+    <meta name="description" content="Pagina de deportes">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+    <link rel="apple-touch-icon" href="images/milogo2.png">
+    <link rel="shortcut icon" href="images/milogo2.png">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -34,7 +34,7 @@
             <div class="container">
                 <div class="login-content">
                     <div class="login-logo">
-                        <img class="align-content" src="images/milogo.jpg" alt="" width="350" height="auto">
+                        <img class="align-content" src="images/milogo2.png" alt="" width="500" height="auto">
                     </div>
                     <div class="login-form">
                             <div class="form-group">
